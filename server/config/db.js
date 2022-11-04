1fe21f3e541764a66ab5a9037f3b8e62ce6e72f9
ManHonCheung <113254862+ManHonCheung@users.noreply.github.com>
2022-11-04 03:32:18 -0400

    Create db.js

---
+++ server/config/db.js
@@ -0,0 +1,9 @@
+module.exports = 
+{
+    //"URI": "mongodb://localhost/book_store"
+   // "URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority",
+   "Secret": 'SomeSecret',
+
+   "URI": "mongodb+srv://cheung:leo19921010@cluster0.agrxcjq.mongodb.net/test" 
+
+}
